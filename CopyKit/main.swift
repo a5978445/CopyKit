dbed78b5df0c842831fa043f11b4154d5ea51e33
fitch <510491354@qq.com>
2020-04-27 16:33:00 +0800

    Initial Commit

---
+++ CopyKit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CopyKit
+//
+//  Created by 李腾芳 on 2020/4/27.
+//  Copyright © 2020 HSBC Holdings plc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
